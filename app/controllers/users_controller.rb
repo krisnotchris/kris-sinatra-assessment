7082c2f45e246de74e92327f8f2b21a97e5528fb
krisnotchris <krishenderson@gmail.com>
2016-07-01 18:01:29 -0500

    debugging database persistence

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+
   get '/signup' do 
     if logged_in?
       redirect '/workouts'
@@ -12,22 +13,28 @@ class UsersController < ApplicationController
     if params[:username] == "" || params[:password] == ""
       redirect '/signup'
     else
-      @user = User.new(params[:user])
-      @user.save
+      @user = User.create(params[:user])
       session[:user_id] = @user.id
-      
       redirect '/workouts'
     end
   end
 
   post '/login' do 
-    @user = User.find_by(:username => params[:username])
-    if @user && @user.authenticate(params[:password])
+    binding.pry
+    @user = User.find_by(:username => params[:user][:username])
+
+    
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect '/workouts'
     else
       redirect '/'
     end
+  end
+
+  get '/logout' do 
+    session.clear
+    redirect '/'
   end
 
 end
