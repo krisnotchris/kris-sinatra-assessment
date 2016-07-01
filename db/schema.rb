7ef402ab8ff9e500588c2adb45466565b260460f
krisnotchris <krishenderson@gmail.com>
2016-07-01 16:08:47 -0500

    got the associations working

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701204614) do
+ActiveRecord::Schema.define(version: 20160701205001) do
 
   create_table "users", force: true do |t|
     t.string "username"
     t.string "password_digest"
+  end
+
+  create_table "workouts", force: true do |t|
+    t.string  "name"
+    t.integer "weight"
+    t.integer "reps"
+    t.integer "user_id"
   end
 
 end
